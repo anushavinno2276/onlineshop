135265a58ab8e6aed5828ae2616332c15ad10392
Anusha <anusha.vinno@gmail.com>
2018-08-20 15:54:07 +0530

    deleted from branch 2

--- src/org/online/shopping/service/Test.java
+++
@@ -1,8 +0,0 @@
-package org.online.shopping.service;
-
-public class Test {
-	public static void main(String[] args) {
-		System.out.println("Hello JAVA");
-		System.out.println("Hello JAVA");
-	}
-}
